9d3ff246147928f263e706f52c6920ecce383025
Katrainer <laub94@list.ru>
2021-11-10 22:12:11 +0300

    making task 8 button 'sort down' don't work :(

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,37 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8"
+
+type GeneralType = SortType | CheckType
+type SortType = {
+    type: 'sort'
+    payload: 'up' | 'down'
+}
+
+type CheckType = {
+    type: 'check'
+    payload: number
+}
+
+export const homeWorkReducer = (state: Array<UserType>, action: GeneralType): Array<UserType> => { // need to fix any
+
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            const sortArray = (x: any, y: any) => {
+                if (x.name < y.name) {
+                    return -1;
+                }
+                if (x.name > y.name) {
+                    return 1;
+                }
+                return 0;
+            }
+            return action.payload === 'up' ? [...state.sort(sortArray)] : [...state.sort(sortArray).reverse()]
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(t => t.age > action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
+
+
